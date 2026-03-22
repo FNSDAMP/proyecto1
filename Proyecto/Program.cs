f577ae25f5bb2e456642b81fe445fac6bf79a1cd
Diego <diegovfunes@gmail.com>
2026-03-22 09:09:47 -0600

    commit 2: En este commit agregue las funciones entro del switch que va a averificar el menu, y el do while que va a cerrar el sistema cuabdo se seleccione la opcion de salir. Tambien un ciclo for, su funcion solo va a ser dibujar una linea en el menu. Y la opcion que vam a evaluar el contenido donde se le pedira al usuario que ingrese la informacion para poder verificarla, y todo se va a air acumulando en el contador para que se pueda ver el total de evaluados.

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -20,7 +20,9 @@ namespace SimuladorStreaming
             do
             {
                 Console.Clear(); //Este comando limpia la pantalla, en este caso el menu cada vez que se vuelve a repetir el ciclo.
+                DibujarLinea();
                 Console.WriteLine("   SISTEMA DE PLATAFORMA DE STREAMING");
+                DibujarLinea();
                 Console.WriteLine("1. Evaluar nuevo contenido");
                 Console.WriteLine("2.Mostrar reglas del sistema");
                 Console.WriteLine("3.Ver estadisticas");
@@ -47,12 +49,91 @@ namespace SimuladorStreaming
 
                 // 4. switch verifica las opciones del menu
                 switch (opcion)
-                { 
-                    //Aqui van a ir las funciones del menu
+                {
+                    case 1:
+                        EvaluarContenido();
+                        break;
+                    case 2:
+                        MostrarReglas();
+                        break;
+                    case 3:
+                        MostrarEstadisticas();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nCerrando el sistema...");
+                        break;
+
+                }
+                // Pausa antes de limpiar la pantalla
+                if (opcion != 4)
+                {
+                    Console.WriteLine("\nPresione ENTER para continuar...");
+                    Console.ReadLine();
                 }
 
-             } while (opcion != 4) ;
-            
+                //Mientras que la opcion sea diferente que 4 se ejecutara 
+
+
+            } while (opcion != 4);
+
+        }
+        // 5. Este ciclo for solo va a diujar una linea que es para decoracion en el menu
+        static void DibujarLinea()
+        {
+            for (int i = 0; i < 45; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+        // 6. Esta funcion va a evaluar las opciones
+        static void EvaluarContenido()
+        {
+            Console.Clear();
+            Console.WriteLine("--- EVALUAR CONTENIDO ---");
+
+            // Aqui se pedira al usuario que seleccione una opcion de manera sencilla, con numeros
+            Console.Write("Tipo ---->       1----Pelicula, 2----Serie, 3----Documental, 4----En vivo ");
+            int tipo;
+            while (!int.TryParse(Console.ReadLine(), out tipo))
+            {
+                Console.Write("Dato inválido. Ingrese el número del tipo. ");
+            }
+
+            Console.Write("Duracion en minutos: ");
+            int duracion;
+            while (!int.TryParse(Console.ReadLine(), out duracion))
+            {
+                Console.Write("Dato invalido. Ingrese la duracion en numeros. ");
+            }
+
+            Console.Write("Clasificacion ---->      1----Todo publico, 2---- +13, 3---- +18 ");
+            int clasificacion;
+            while (!int.TryParse(Console.ReadLine(), out clasificacion))
+            {
+                Console.Write("Dato invalido. Ingrese el número de la clasificacion. ");
+            }
+
+            Console.Write("Hora de programación (0 a 23): ");
+            int hora;
+            while (!int.TryParse(Console.ReadLine(), out hora))
+            {
+                Console.Write("Dato inválido. Ingrese la hora en numeros. ");
+            }
+
+            Console.Write("Nivel de producción ---->     1---- Baja, 2---- Media, 3---- Alta ");
+            int produccion;
+            while (!int.TryParse(Console.ReadLine(), out produccion))
+            {
+                Console.Write("Dato invqlido. Ingrese el número de la produccion. ");
+            }
+
+            totalEvaluados++;
+            bool esValido = true;
+            string motivoRechazo = "";
+
+
         }
     }
 }
